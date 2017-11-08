deac939b0b787e0bbfe696c29844dccecf99afce
Robert van Steen <hello@rovansteen.nl>
2017-11-08 17:18:10 +0100

    Add executeQuery to Bakery class.

--- src/Bakery.php
+++ src/Bakery.php
@@ -3,6 +3,10 @@
 namespace Scrn\Bakery;
 
 use Exception;
+use GraphQL\GraphQL;
+use GraphQL\Type\Schema;
+use GraphQL\Executor\ExecutionResult;
+use GraphQL\Type\Definition\ObjectType;
 use Scrn\Bakery\Exceptions\ModelNotRegistered;
 
 class Bakery
@@ -29,5 +33,33 @@ class Bakery
         }
 
         return app($model)->toObjectType();
+    }
+
+    /**
+     * Execute the GraphQL query.
+     *
+     * @param array $input
+     * @return ExecutionResult
+     */
+    public function executeQuery($input): ExecutionResult
+    {
+        $schema = new Schema([
+            'query' => new ObjectType([
+                'name' => 'Query',
+                'fields' => [],
+            ]),
+            'mutation' => new ObjectType([
+                'name' => 'Mutation',
+                'fields' => [],
+            ]),
+        ]);
+
+        $root = null;
+        $context = null;
+        $query = array_get($input, 'query');
+        $variables = json_decode(array_get($input, 'variables'));
+        $operationName = array_get($input, 'operationName');
+
+        return GraphQL::executeQuery($schema, $query, $root, $context, $variables, $operationName);
     }
 }
--- src/Http/Controller/BakeryController.php
+++ src/Http/Controller/BakeryController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\JsonResponse;
-use GraphQL\Executor\ExecutionResult;
-use GraphQL\Type\Definition\ObjectType;
 
 class BakeryController extends Controller
 {
@@ -33,36 +31,8 @@ class BakeryController extends Controller
     {
         $input = $request->all();
 
-        $schema = new Schema([
-            'query' => new ObjectType([
-                'name' => 'Query',
-                'fields' => [],
-            ]),
-            'mutation' => new ObjectType([
-                'name' => 'Mutation',
-                'fields' => [],
-            ]),
-        ]);
-        $data = $this->executeQuery($schema, $input);
+        $data = app('bakery')->executeQuery($input);
 
         return response()->json($data, 200, []);
-    }
-
-    /**
-     * Execute the GraphQL query and return the result.
-     *
-     * @param Schema $schema
-     * @param array $input
-     * @return ExecutionResult
-     */
-    protected function executeQuery(Schema $schema, $input = []): ExecutionResult
-    {
-        $root = null;
-        $context = null;
-        $query = array_get($input, 'query');
-        $variables = json_decode(array_get($input, 'variables'));
-        $operationName = array_get($input, 'operationName');
-
-        return GraphQL::executeQuery($schema, $query, $root, $context, $variables, $operationName);
     }
 }
