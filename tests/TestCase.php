9932330f058d8b6b65f7b9d48f259ce8a7a5855c
Erik Gaal <me@erikgaal.nl>
2017-11-09 14:27:18 +0100

    Add Facade and example configuration to base TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,9 @@
 namespace Scrn\Bakery\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Scrn\Bakery\BakeryFacade;
 use Scrn\Bakery\BakeryServiceProvider;
+use Scrn\Bakery\Tests\Stubs\Model;
 
 class TestCase extends OrchestraTestCase
 {
@@ -13,8 +15,22 @@ class TestCase extends OrchestraTestCase
         $this->withoutExceptionHandling();
     }
 
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('bakery.models', [
+            Model::class,
+        ]);
+    }
+
     protected function getPackageProviders($app)
     {
         return [BakeryServiceProvider::class];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Bakery' => BakeryFacade::class,
+        ];
     }
 }
