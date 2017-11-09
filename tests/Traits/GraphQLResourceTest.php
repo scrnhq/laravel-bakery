9a9b2dc7f9f403baf71f3cdd4a85ff0872f86514
Erik Gaal <me@erikgaal.nl>
2017-11-09 14:26:13 +0100

    Fix the GraphQLResource tests

--- tests/Traits/GraphQLResourceTest.php
+++ tests/Traits/GraphQLResourceTest.php
@@ -2,35 +2,16 @@
 
 namespace Scrn\Bakery\Tests\Traits;
 
-use GraphQL\Type\Definition\ObjectType;
-use GraphQL\Type\Definition\Type;
-use Scrn\Bakery\Tests\Stubs\EmptyModel;
 use Scrn\Bakery\Tests\Stubs\Model;
 use Scrn\Bakery\Tests\TestCase;
 
 class GraphQLResourceTest extends TestCase
 {
     /** @test */
-    public function it_builds_an_object_type_from_an_empty_model()
+    public function it_has_fields()
     {
-        $expected = new ObjectType([
-            'name' => 'EmptyModel',
-            'fields' => [],
-        ]);
+        $fields = app(Model::class)->fields();
 
-        $this->assertEquals($expected, app(EmptyModel::class)->toObjectType());
-    }
-
-    /** @test */
-    public function it_builds_an_object_type_from_a_model_with_fields()
-    {
-        $expected = new ObjectType([
-            'name' => 'Model',
-            'fields' => [
-                'id' => Type::ID(),
-            ],
-        ]);
-
-        $this->assertEquals($expected, app(Model::class)->toObjectType());
+        $this->assertInternalType('array', $fields);
     }
 }
