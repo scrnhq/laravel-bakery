1462c4f6c8299e7c1e6d7d8184b9b998d7bfe230
Robert van Steen <hello@rovansteen.nl>
2018-01-18 17:51:30 +0100

    Unwrap input types from `nonNull` for update input type.

--- src/Types/UpdateInputType.php
+++ src/Types/UpdateInputType.php
@@ -4,6 +4,7 @@ namespace Bakery\Types;
 
 use GraphQL\Type\Definition\Type;
 use Bakery\Support\Facades\Bakery;
+use GraphQL\Type\Definition\NonNull;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations;
 
@@ -51,12 +52,16 @@ class UpdateInputType extends InputType
      */
     private function getFillableFields(): array
     {
-        return array_filter($this->model->fields(), function ($value, $key) {
+        $fields = array_filter($this->model->fields(), function ($value, $key) {
             $type = Type::getNamedType($value);
             $fillable = array_values($this->model->getFillable());
 
             return in_array($key, $fillable) && Type::isLeafType($type);
         }, ARRAY_FILTER_USE_BOTH);
+
+        return array_map(function ($type) {
+            return $type instanceof NonNull ? $type->getWrappedType() : $type;
+        }, $fields);
     }
 
     /**
