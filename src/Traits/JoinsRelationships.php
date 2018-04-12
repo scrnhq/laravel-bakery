379756c5503810fc210bf507a52d483bf6f730ec
Erik Gaal <me@erikgaal.nl>
2018-04-12 16:22:18 +0200

    Fix laravel 5.4 support for joining relations

--- src/Traits/JoinsRelationships.php
+++ src/Traits/JoinsRelationships.php
@@ -22,8 +22,13 @@ trait JoinsRelationships
         if ($relation instanceof Relations\BelongsTo) {
             $query->join($related->getTable(), $relation->getQualifiedOwnerKeyName(), '=', $relation->getQualifiedForeignKey(), $type, $where);
         } elseif ($relation instanceof Relations\BelongsToMany) {
-            $query->join($relation->getTable(), $relation->getQualifiedParentKeyName(), '=', $relation->getQualifiedForeignPivotKeyName(), $type, $where);
-            $query->join($related->getTable(), $relation->getQualifiedRelatedPivotKeyName(), '=', $related->getQualifiedKeyName(), $type, $where);
+            $foreignPivotKeyName = method_exists($relation, 'getQualifiedForeignPivotKeyName')
+                ? $relation->getQualifiedForeignPivotKeyName() : $relation->getQualifiedForeignKeyName();
+            $relatedPivotKeyName = method_exists($relation, 'getQualifiedRelatedPivotKeyName')
+                ? $relation->getQualifiedRelatedPivotKeyName() : $relation->getQualifiedRelatedKeyName();
+
+            $query->join($relation->getTable(), $relation->getQualifiedParentKeyName(), '=', $foreignPivotKeyName, $type, $where);
+            $query->join($related->getTable(), $relatedPivotKeyName, '=', $related->getQualifiedKeyName(), $type, $where);
         } elseif ($relation instanceof Relations\HasMany) {
             $query->join($related->getTable(), $relation->getQualifiedForeignKeyName(), '=', $relation->getQualifiedParentKeyName(), $type, $where);
         }
