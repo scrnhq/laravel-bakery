f8b03c43229b397c23e4ffe428975f2be0f0bfd5
Erik Gaal <me@erikgaal.nl>
2018-09-10 12:41:50 +0200

    Add test for observables

--- tests/Traits/MutableTest.php
+++ tests/Traits/MutableTest.php
@@ -19,12 +19,21 @@ class MutableTest extends FeatureTestCase
     }
 
     /** @test */
-    public function it_dispates_the_persisted_event_for_a_mutable()
+    public function it_dispatches_the_persisted_event_for_a_mutable()
     {
         Event::fake();
 
         factory(Article::class)->create();
 
         Event::assertDispatched('eloquent.persisted: '.Article::class);
+    }
+
+    /** @test */
+    public function it_adds_the_persist_events_to_the_observables()
+    {
+        /** @var \Illuminate\Database\Eloquent\Model $article */
+        $article = factory(Article::class)->create();
+
+        $this->assertArraySubset(['persisting', 'persisted'], $article->getObservableEvents());
     }
 }
