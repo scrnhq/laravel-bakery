d9eab77e7a16bd14c55aa055cf5e9d0d6f3e870e
Erik Gaal <me@erikgaal.nl>
2018-09-24 14:30:37 +0200

    Remove unused utilities

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -42,23 +42,4 @@ class Utils
     {
         return studly_case(str_plural(class_basename($class)));
     }
-
-    public static function pluralRelationship($relationship)
-    {
-        return $relationship instanceof Relations\HasMany ||
-               $relationship instanceof Relations\BelongsToMany;
-    }
-
-    public static function singularRelationship($relationship)
-    {
-        return $relationship instanceof Relations\BelongsTo ||
-               $relationship instanceof Relations\HasOne;
-    }
-
-    public static function usesTrait($class, string $trait)
-    {
-        $traits = class_uses_deep($class, true);
-
-        return in_array($trait, $traits);
-    }
 }
