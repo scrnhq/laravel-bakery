8b9d44135a2385aabfdf70686afcc851330f8d3a
Erik Gaal <me@erikgaal.nl>
2018-11-13 14:59:58 +0100

    Resolve schema as non mutable if there are no fillable fields

--- src/Eloquent/ModelSchema.php
+++ src/Eloquent/ModelSchema.php
@@ -147,6 +147,10 @@ abstract class ModelSchema
      */
     public function isMutable()
     {
+        if (! $this->getFillableFields()->count()) {
+            return false;
+        }
+
         return $this->mutable;
     }
 
