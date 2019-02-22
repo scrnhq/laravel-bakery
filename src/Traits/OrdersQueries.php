28c5f955663b0f7c5d8f72b6261a96d221257fed
Erik Gaal <me@erikgaal.nl>
2019-02-22 14:15:38 +0100

    Use distinct with correct selects instead of grouping

--- src/Traits/OrdersQueries.php
+++ src/Traits/OrdersQueries.php
@@ -72,6 +72,7 @@ trait OrdersQueries
      */
     protected function orderBy(Builder $query, string $column, string $ordering)
     {
+        $query->addSelect($column);
         return $query->orderBy($column, $ordering);
     }
 }
